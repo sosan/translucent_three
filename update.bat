adebb9ab9bfbef95d193532b704510076b2358ff
jose <josloe@gmail.com>
2020-07-26 21:37:24 +0200

    fixed node.yml and update.bat

--- update.bat
+++ update.bat
@@ -3,3 +3,4 @@ ECHO --- CON CMDER ---
 cp node_modules/three/build/three.module.js src/programs
 cp node_modules/three/examples/jsm/controls/OrbitControls.js src/programs
 cp node_modules/three/examples/jsm/libs/dat.gui.module.js src/programs
+cp node_modules/three/examples/jsm/loaders/GLTFLoader.js src/programs

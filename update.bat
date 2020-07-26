76dacd932be7e0161b23defd9b75e8901dea078e
jose <josloe@gmail.com>
2020-07-26 21:33:59 +0200

    added update

---
+++ update.bat
@@ -0,0 +1,5 @@
+@echo off
+ECHO --- CON CMDER ---
+cp node_modules/three/build/three.module.js src/programs
+cp node_modules/three/examples/jsm/controls/OrbitControls.js src/programs
+cp node_modules/three/examples/jsm/libs/dat.gui.module.js src/programs

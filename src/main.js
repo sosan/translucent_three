89f6d36648f44cb5076a1f5880359e85ef258c9a
jose <josloe@gmail.com>
2020-07-26 19:03:44 +0200

    added tren

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ function init()
     });
 
     let mesh;
-    new GLTFLoader().load("../assets/statue.glb",
+    new GLTFLoader().load("../assets/tren.glb",
         function ( gltf ) {
             mesh = gltf.scene.children[0];
             mesh.position.set(0,0,0);

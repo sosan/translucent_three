7ad8bd2ba5de10400888f760dbc7aacfd4706709
jose <josloe@gmail.com>
2020-07-26 20:00:23 +0200

    added corset

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ function init()
     });
 
     let mesh;
-    new GLTFLoader().load("../assets/ducko.glb",
+    new GLTFLoader().load("../assets/corseto.glb",
         function ( gltf ) {
             mesh = gltf.scene.children[0];
             mesh.position.set(0,0,0);

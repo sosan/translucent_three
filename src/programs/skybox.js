3216c0b2e3c2e014fbd36e908b41e04642e9bfae
jose <josloe@gmail.com>
2020-07-26 18:45:40 +0200

    fixed main.js

--- src/programs/skybox.js
+++ src/programs/skybox.js
@@ -1,4 +1,4 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
+import * as THREE from "three.module.js";
 
 class Skybox {
     constructor(texture, camera, renderer, args) {

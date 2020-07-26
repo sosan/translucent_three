1a3059afdfe675e5a9d6e025f6157b29cf4c3661
jose <josloe@gmail.com>
2020-07-26 20:05:15 +0200

    added aguacate

--- src/main.js
+++ src/main.js
@@ -79,7 +79,7 @@ function init()
     });
 
     let mesh;
-    new GLTFLoader().load("../assets/corseto.glb",
+    new GLTFLoader().load("../assets/aguacate.glb",
         function ( gltf ) {
             mesh = gltf.scene.children[0];
             mesh.position.set(0,0,0);
